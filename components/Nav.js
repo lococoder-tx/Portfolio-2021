ae975c75bd33508b3db491567a8c27b150012f1c
chaleay <elijahgaytan@utexas.edu>
2021-03-11 21:22:42 -0600

    got nav bar working

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 const Nav = () => {
+  const [navBarOn, setNavBarOn] = useState(false);
+
   return (
     <div className="navigation">
-      <div className="navigation__button">
+      <div
+        className="navigation__button"
+        onClick={() => {
+          setNavBarOn(!navBarOn);
+        }}
+      >
         <span className="navigation__icon"></span>
       </div>
-      <div className="navigation__bar"></div>
+      <CSSTransition classNames="fade" in={navBarOn} timeout={1000}>
+        <>
+          <div className={`navigation__bar`}></div>
+        </>
+      </CSSTransition>
     </div>
   );
 };

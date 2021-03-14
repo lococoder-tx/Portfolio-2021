0455bfa2343b0ff242d6b11fe9a2a14f242e3fdf
chaleay <elijahgaytan@utexas.edu>
2021-03-14 03:57:33 -0500

    added transitions

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -6,8 +6,21 @@ const Home = () => {
       <h1 className="title">Elijah Gaytan</h1>
       <h6 className="sub-title">Full-Stack Engineer | Linux Enthusiast</h6>
       <ul className="social_media_container">
-        <li>Btn 1</li>
-        <li>Btn 2</li>
+        <li>
+          <a href="https://github.com/chaleay">
+            <i aria-hidden className="fab fa-github"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/chaleay">
+            <i aria-hidden className="fab fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/chaleay">
+            <i aria-hidden className="fab fa-twitter"></i>
+          </a>
+        </li>
       </ul>
     </div>
   );

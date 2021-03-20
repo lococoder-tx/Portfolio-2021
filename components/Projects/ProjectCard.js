109ff8d97a1d5683cd3c6b1bbe6ccdb55b4c0199
chaleay <elijahgaytan@utexas.edu>
2021-03-20 15:41:41 -0500

    made good progress on card

--- components/Projects/ProjectCard.js
+++ components/Projects/ProjectCard.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ProjectCard = () => {
-  return <div className="projects__card"></div>;
+  return (
+    <div className="projects__card">
+      <div className="projects__card__image">
+        <Image width={1477} height={600} src="/valhalla.png"></Image>
+      </div>
+      <h1 className="projects__card__header">Valhalla Marketplace</h1>
+      <div className="projects__card__desc">
+        <p>
+          A marketplace I created to help small vendors sell their merchandise.
+          Basically, it's eBay.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProjectCard from '../components/Projects/ProjectCard';
 
 const projects = () => {
   return (
     <main className="container">
       <section className="projects">
-        <div className="projects__card"></div>
+        <ProjectCard></ProjectCard>
       </section>
     </main>
   );

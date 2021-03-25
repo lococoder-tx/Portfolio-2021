d1c93ded66a92ea43bd778e550dab211fb3afb43
chaleay <elijahgaytan@utexas.edu>
2021-03-25 01:07:02 -0500

    some minor adjustments

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import SocialMediaContainer from '../components/SocialMediaContainer';
 
 const contact = () => {
   return (
     <main className="container">
       <section className="contact-me">
         <h1>&rarr; Contact Me &larr;</h1>
-        <div className="contact-me__box"></div>
+        <SocialMediaContainer />
       </section>
     </main>
   );
